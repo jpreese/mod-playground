62f211aba0d6a08513e0a5a5b9f798334e4ea27a
John Reese <john@reese.dev>
2019-07-30 23:18:57 -0400

    Initial

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/jpreese/mod-playground
+
+go 1.12
+
+require (
+	github.com/mattn/go-isatty v0.0.7
+)
---
+++ main.go
@@ -0,0 +1,10 @@
+package playground
+
+import (
+	"fmt"
+	"github.com/mattn/go-isatty"
+)
+
+func main() {
+	fmt.Println("mine")
+}
